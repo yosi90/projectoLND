66a60cf0660682e9d75be51bce529e4788c87f34
yosi90 <yosi121990@hotmail.es>
2023-02-09 23:27:15 +0000

    map RC1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
 $('#menu').toggle();
+$("#textoCabecera").toggle();
+$("#textoArte").toggle();
+$("#textoTipo").toggle();
+$("#textoReglas").toggle();
+$("#textoEstadisticas").toggle();
 
-$('#btnMenu').click(function (e) { 
+$('#btnMenu').click(function (e) {
     e.preventDefault();
     $('#menu').toggle('slow');
 });
+
+$(".cabeceraCarta").hover(function () {
+    $("#textoCabecera").toggle();
+});
+
+$(".arteCarta").hover(function () {
+    $("#textoArte").toggle();
+});
+
+$(".tipoCarta").hover(function () {
+    $("#textoTipo").toggle();
+});
+
+$(".reglasCarta").hover(function () {
+    $("#textoReglas").toggle();
+});
+
+$(".estadisticasCarta").hover(function () {
+    $("#textoEstadisticas").toggle();
+});
+
+$('#mapa area').hover(
+    function() {
+      var coords = $(this).attr('coords').split(',');
+      var width = $('#desglose').width();
+      var height = $('#desglose').height();
+      $('#desglose .map-selector').addClass('hover').css({
+        'left': coords[0] + 'px',
+        'top': coords[1] + 'px',
+        'right': width - coords[2],
+        'bottom': height - coords[3]
+      })
+    },
+    function() {
+      $('#desglose .map-selector').removeClass('hover').attr('style', '');
+    }
+  )
